f0695beb8a1bd7545728d305538f9ce3ef0085f1
soraew <soraew@gmail.com>
2024-01-07 17:48:59 +0900

    plot TOTAL and 764

--- china_usa_trade/src/12_works/12_22_vis.py
+++ china_usa_trade/src/12_works/12_22_vis.py
@@ -20,9 +20,8 @@ from hfuncs.plotting_communities import *
 # # load data
 csvs_root= '../../csvs/'
 data_root= '../../../../data/trade/BACI/'
-data_file_name='top_ex_imp_2017_21_new.csv'
+data_file_name='total_764_2016_21.csv'
 
-# ONLY EXPORTS
 products = get_product_data(
     exports=True,
     imports=True,
@@ -49,7 +48,7 @@ products = products_labeled.copy()
 del(products_economy_labeled)
 del(products_labeled)
 # %%
-products = products[products['Level']==3]
+# products = products[products['Level']==3]
 products = products.dropna(subset=['Code_economy', 'Code_partner'])
 products = products[products['partner_label'] != products['economy_label']]
 # products = products[products['flow']==2]
@@ -74,20 +73,23 @@ def get_filter(products, product_code='764', year=2017, flow=False, percentile=0
 # %% [markdown]
 # # Plotting communities over years
 # %%
-FLOW = 1 # 1: imports, 2: exports
+# %%
+FLOW = 1# 1: imports, 2: exports
 FIGSIZE = (21, 21)
-KUSD_FILTER = \
-    get_filter(
-        products, product_code='764',
-        year=2017, flow=FLOW, percentile=0.9)
-print('kusd_filter: ', KUSD_FILTER)
-YEARS = (2017, 2019, 2021)
+YEARS = (2016, 2017, 2019) 
+
 PRODUCTS = [
+    'TOTAL',
     '764', # Telecommunication equipment, n.e.s. & parts, ...
     # '776', # Cathode valves & tubes(semiconductors)
     # '778', # Electrical machinery & apparatus, n.e.s.
 ]
 for product in PRODUCTS:
+    KUSD_FILTER = \
+        get_filter(
+            products, product_code=product,
+            year=2016, flow=FLOW, percentile=0.95)
+    print('kusd_filter: ', KUSD_FILTER)
     plot_communities_years(
         years=YEARS,
         product_code=product,
@@ -95,9 +97,11 @@ for product in PRODUCTS:
         # product_df=products,
         kusd_filter=KUSD_FILTER,
         figsize=FIGSIZE,
-        label_columns=['Code_economy', 'Code_partner']
+        label_columns=['Code_economy', 'Code_partner'],
         # label_columns=['economy_label', 'partner_label']
+        # detection_type='gm',
         )
+
 
 # %% [markdown]
 # # Plotting top partners over years
@@ -128,13 +132,13 @@ def get_top_partners_over_years(
 # %% [markdown]
 # ## Plot top partners over years
 # %%
-# EXPORTERS = [ "China", "Korea, Republic of", "Viet Nam", "China, Taiwan Province of", "Japan", "India", ]
 plot_top_partners = False
 if plot_top_partners:
     YEARS = (2017, 2018, 2019, 2021)
     PRODUCT_CODE = '764'
     ECONOMY = "United States of America"
     # ECONOMY = "China"
+    ECONOMY = "Japan"
     FLOW = 1
     N = 5
     product_df = products.copy()
@@ -155,12 +159,13 @@ if plot_top_partners:
 
         tmp_products = products[all_filters].copy()
         tmp_products['year'] = tmp_products['year'].astype(str)
+        flow_name = 'Exports' if FLOW==2 else 'Imports'
         fig = px.line(
             tmp_products,
             x='year',
             y='KUSD',
             color='partner_label',
-            title=f'{product} Trade',
+            title=f'{product} Top {N} {flow_name} partners of {ECONOMY}',
             markers=True,
             )
         fig.show()
