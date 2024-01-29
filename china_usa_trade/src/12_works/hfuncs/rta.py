76db0d59cb6ab3c23d6757c120e731ad5620d850
soraew <soraew@gmail.com>
2024-01-29 13:05:48 +0900

    fixed generating rta per year

--- china_usa_trade/src/12_works/hfuncs/rta.py
+++ china_usa_trade/src/12_works/hfuncs/rta.py
@@ -6,7 +6,7 @@ from warnings import filterwarnings
 filterwarnings('ignore')
 
 from hfuncs.plotting_communities import plot_basic_communities, \
-    get_louvain_partition
+    get_louvain_partition, plot_communities_geo
 from hfuncs.others import find_fuzz
 
 # funtions for preprocessing rta data
@@ -84,7 +84,6 @@ def replace_abbvs_with_countries(
     tmp_rta_new = tmp_rta.copy()
     tmp_rta_new['Current signatories'] = tmp_rta['Current signatories'].\
         apply(lambda string: replace_abbv_with_country(string, abbv_rta_dict))
-    # breakpoint()
     return tmp_rta_new
 
 # replace abbreviations with countries in rta df
@@ -99,37 +98,35 @@ def abbv_to_countries(tmp_rta, abbv_rta_dict):
 def get_rta_year(tmp_rta_new, year='2017', git_csvs_root='../../csvs_git/'):
     rta_year = tmp_rta_new.copy()
     sign_pre_year_filter = \
-        (rta_year['date_of_sign_G'] < pd.to_datetime(f'{str(year)}-01-01')).values
+        (rta_year['date_of_sign_G'] <= pd.to_datetime(f'{str(year)}-07-02')).values
     rta_year = rta_year[sign_pre_year_filter]
-    if year == '2017':
-        breakpoint()
-    # from 2017
     if int(year) <= 2016:
         rta_year.at[692,'Current signatories'] = \
             rta_year.at[692,'Current signatories']\
                 .replace('Ecuador;', '')
-    # from 2018
-    if int(year) <= 2017:
-        # rta_year.at[953,'Current signatories'] = \
-        #     rta_year.at[953,'Current signatories']\
-        #         .replace('Liechtenstein;', '').replace('Switzerland;', '')
+    # # from 2018
+    if int(year) == 2017:
+        rta_year.at[953,'Current signatories'] = \
+            rta_year.at[953,'Current signatories']\
+                .replace('Liechtenstein;', '').replace('Switzerland;', '')
         rta_year.at[897, 'Current signatories'] = \
             rta_year.at[897, 'Current signatories']\
-                .replace('Mozambique;', '')# done tile here
+                .replace('Mozambique;', '')
     # from 2019
     if int(year) <= 2018:
         rta_year.at[469,'Current signatories'] = \
             rta_year.at[469,'Current signatories']\
                 .replace('Comoros;', '')
-        rta_year.at[640,'Current signatories'] = \
-            rta_year.at[640,'Current signatories']\
-                .replace('Viet Nam;', '')
-        rta_year.at[901,'Current signatories'] = \
-            rta_year.at[901,'Current signatories']\
-                .replace('Costa Rica', '')
-        rta_year.at[994,'Current signatories'] = \
-            rta_year.at[994,'Current signatories']\
-                .replace('Malaysia', '')
+        if int(year) == 2018:
+            rta_year.at[640,'Current signatories'] = \
+                rta_year.at[640,'Current signatories']\
+                    .replace('Viet Nam;', '')
+            rta_year.at[901,'Current signatories'] = \
+                rta_year.at[901,'Current signatories']\
+                    .replace('Costa Rica', '')
+            rta_year.at[994,'Current signatories'] = \
+                rta_year.at[994,'Current signatories']\
+                    .replace('Malaysia', '')
     # from 2020
     if int(year) <= 2019:
         rta_year.at[759,'Current signatories'] = \
@@ -137,43 +134,56 @@ def get_rta_year(tmp_rta_new, year='2017', git_csvs_root='../../csvs_git/'):
                 .replace('Solomon Islands;', '')
     # # from 2021, 2022
     if int(year) <= 2020:
-        rta_year.at[640,'Current signatories'] = \
-            rta_year.at[640,'Current signatories']\
-                .replace('Peru;', '').replace('Malaysia;', '')
-        rta_year.at[901,'Current signatories'] = \
-            rta_year.at[901,'Current signatories']\
-                .replace('Panama;', '')
-        rta_year.at[994,'Current signatories'] = \
-            rta_year.at[994,'Current signatories']\
+        if int(year) >= 2018:
+            rta_year.at[640,'Current signatories'] = \
+                rta_year.at[640,'Current signatories']\
+                    .replace('Peru;', '').replace('Malaysia;', '')
+            rta_year.at[901,'Current signatories'] = \
+                rta_year.at[901,'Current signatories']\
+                    .replace('Panama;', '')
+            rta_year.at[994,'Current signatories'] = \
+                rta_year.at[994,'Current signatories']\
                 .replace('Cambodia;', '')
-        rta_year.at[1111,'Current signatories'] = \
-            rta_year.at[1111,'Current signatories']\
-                .replace('Solomon Islands;', '').replace('Samoa;', '')
+        if int(year) >= 2019:
+            rta_year.at[1111,'Current signatories'] = \
+                rta_year.at[1111,'Current signatories']\
+                    .replace('Solomon Islands;', '').replace('Samoa;', '')
+            
     # til 2021
     # United Kingdom
     if int(year) <= 2020:
         # til_2021 = pd.read_csv(git_csvs_root + 'tmp_find.csv', index_col=0)
-        til_2021=[6,12,18,33,44,52,63,68,73,77,91,92,93,107,109,111,112,114,
+        til_2021_uk=[6,12,18,33,44,52,63,68,73,77,91,92,93,107,109,111,112,114,
               118,123,132,137,138,142,143,144,145,148,154,167,386,564,605,619,
               623,680,712,836,847,848,849,850,869,872, 907,1002,1093,1094,1148]
-        for i in til_2021.index:
-            if 'United Kingdom' in rta_year.at[i,'Current signatories']:
+        for i in til_2021_uk:
+            try:
+                if 'United Kingdom' in rta_year.at[i,'Current signatories']:
+                    pass
+            except:
                 pass
             else:
-                rta_year.at[i,'Current signatories'] = \
-                    rta_year.at[i,'Current signatories'] + 'United Kingdom;'
-                assert 'United Kingdom' in rta_year.at[i,'Current signatories']
+                try:
+                    rta_year.at[i,'Current signatories'] = \
+                        rta_year.at[i,'Current signatories'] + 'United Kingdom;'
+                    assert 'United Kingdom' in rta_year.at[i,'Current signatories']
+                except:
+                    pass
+
     return rta_year
 
 # GET COUNTRY CODES PER RTA 
 def get_country_codes_per_RTA(tmp_rta_new, rta_to_country_codes):
     country_codes_per_RTA = []
-    all_RTA_ids = tmp_rta_new['RTA ID'].unique()
+    # all_RTA_ids = tmp_rta_new['RTA ID'].unique()
+    all_RTA_ids = tmp_rta_new.index.unique()
     rta_country_codes = set()
     # after adding abbvs
     for RTA_id in all_RTA_ids:
-        tmp_row = tmp_rta_new[tmp_rta_new['RTA ID'] == RTA_id]
-        tmp_str = tmp_row['Current signatories'].values[0]
+        # tmp_row = tmp_rta_new[tmp_rta_new['RTA ID'] == RTA_id]
+        tmp_row = tmp_rta_new.loc[RTA_id]
+        # tmp_str = tmp_row['Current signatories'].values[0]
+        tmp_str = tmp_row['Current signatories']
         if tmp_str is np.nan: # only exception is NAFTA
             print('No Current signatories for:', tmp_row['RTA Name'])
             continue
@@ -245,20 +255,44 @@ if __name__ == '__main__':
 
     # get all RTAs in country codes
     rta_to_country_codes = pd.read_csv(git_csvs_root + 'rta_to_country_codes.csv')
+    rta_country_codes_16, country_codes_per_RTA_16 = \
+        get_country_codes_per_RTA(rta_16, rta_to_country_codes)
     rta_country_codes_17, country_codes_per_RTA_17 = \
         get_country_codes_per_RTA(rta_17, rta_to_country_codes)
     rta_country_codes_18, country_codes_per_RTA_18 = \
         get_country_codes_per_RTA(rta_18, rta_to_country_codes)
     rta_country_codes_19, country_codes_per_RTA_19 = \
         get_country_codes_per_RTA(rta_19, rta_to_country_codes)
+    rta_country_codes_20, country_codes_per_RTA_20 = \
+        get_country_codes_per_RTA(rta_20, rta_to_country_codes)
+    rta_country_codes_21, country_codes_per_RTA_21 = \
+        get_country_codes_per_RTA(rta_21, rta_to_country_codes)
+    rta_16_G = create_rta_network(rta_country_codes_16, country_codes_per_RTA_16)
     rta_17_G = create_rta_network(rta_country_codes_17, country_codes_per_RTA_17)
     rta_18_G = create_rta_network(rta_country_codes_18, country_codes_per_RTA_18)
     rta_19_G = create_rta_network(rta_country_codes_19, country_codes_per_RTA_19)
+    rta_20_G = create_rta_network(rta_country_codes_20, country_codes_per_RTA_20)
+    rta_21_G = create_rta_network(rta_country_codes_21, country_codes_per_RTA_21)
 
+    rta_16_partition = get_louvain_partition(rta_16_G)
     rta_17_partition = get_louvain_partition(rta_18_G)
     rta_18_partition = get_louvain_partition(rta_18_G)
     rta_19_partition = get_louvain_partition(rta_19_G)
-    plot_basic_communities(rta_17_G, rta_17_partition)
-    plot_basic_communities(rta_18_G, rta_18_partition)
-    plot_basic_communities(rta_19_G, rta_19_partition)
+    rta_20_partition = get_louvain_partition(rta_20_G)
+    rta_21_partition = get_louvain_partition(rta_21_G)
+    # plot_basic_communities(rta_16_G, rta_16_partition)
+    # plot_basic_communities(rta_17_G, rta_17_partition)
+    # plot_basic_communities(rta_18_G, rta_18_partition)
+    # plot_basic_communities(rta_19_G, rta_19_partition)
+    iso_corr = \
+        pd.read_csv(
+            git_csvs_root+'countries_iso2to3.csv',
+            encoding="ISO-8859-1",
+            keep_default_na=False) 
+    tmp_dict = {2016: rta_16_partition, 2017: rta_17_partition,
+                2018: rta_18_partition, 2019: rta_19_partition,
+                2020: rta_20_partition, 2021: rta_21_partition}
+    for year, product_partition in tmp_dict.items():
+        plot_communities_geo(product_partition, iso_corr,
+                             'RTA', year, show=False)
 
